69d6f0da1497a70ca7c1ded219c68b72980d2f3d
Christian Alexander Yábar Mejer <chm003@edu.zealand.dk>
2024-09-16 14:16:18 +0200

    done done

---
+++ CalculatorClassLibrary/LommeRegner.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace CalculatorClassLibrary
+{
+    public class LommeRegner
+    {
+
+        /// <summary>
+        /// construktøren
+        /// </summary>
+        public LommeRegner()
+        {
+            
+        }
+
+        /// <summary>
+        /// Plus funktion som tager 2 paremetere
+        /// </summary>
+        /// <param name="x">første parameter i parameterlisten</param>
+        /// <param name="y">anden parameter i parameterlisten</param>
+        /// <returns>retunere svaret</returns>
+        public double PlusFunktion(double x, double y)
+        {
+            
+            return x + y;
+        }
+
+        /// <summary>
+        /// En funktion til brug af minus stykker, så hvis man som individ gerne vil minus to tal så kan man bruge den her funtkion
+        /// </summary>
+        /// <param name="x">første input gemmes som double</param>
+        /// <param name="y">anden input gemmes som double</param>
+        /// <returns>retunere en double med svaret</returns>
+        public double MinusFunktion(double x, double y)
+        {
+            return x - y;
+        }
+
+        /// <summary>
+        /// En funktion til brug af gange stykker, så hvis man har brug for at gange to tal sammen så kan man bruge den her funtkion
+        /// </summary>
+        /// <param name="x">første parameter i parameterlisten</param>
+        /// <param name="y">anden parameter i parameterlisten</param>
+        /// <returns>retunere svaret af regne stykket</returns>
+        public double GangeFunktion(double x, double y)
+        {
+            return x * y;
+        }
+
+        /// <summary>
+        /// En funktion brug af dividere, så det kank man bruge hvis man har lyst
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>retunere de 2 tal divideret med hinanden</returns>
+        public double dividereFunktion(double x, double y)
+        {
+            return x / y;
+        }
+
+
+        
+
+
+
+
+
+
+
+
+    }
+}
---
+++ CalculatorClassLibraryTests/LommeRegnerTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorClassLibrary.Tests
+{
+    [TestClass()]
+    public class LommeRegnerTests
+    {
+        [TestMethod()]
+        public void PlusFunktionTest()
+        {
+            // Arange
+            LommeRegner regner = new LommeRegner();
+
+            // Act
+            double expected = 10;
+            
+            // Assert
+            Assert.AreEqual(expected, regner.PlusFunktion(5,5));
+        }
+
+        [TestMethod()]
+        public void MinusFunktionTest()
+        {
+            // Arange
+            LommeRegner regner = new LommeRegner();
+
+            // Act
+            double expected = 10;
+
+            // Assert
+            Assert.AreEqual(expected, regner.MinusFunktion(20, 10));
+        }
+
+        [TestMethod()]
+        public void GangeFunktionTest()
+        {
+            // Arange
+            LommeRegner regner = new LommeRegner();
+
+            // Act
+            double expected = 100;
+
+            // Assert
+            Assert.AreEqual(expected, regner.GangeFunktion(10,10));
+        }
+
+        [TestMethod()]
+        public void dividereFunktion()
+        {
+            LommeRegner regner = new LommeRegner();
+
+            double expected = 100;
+
+            Assert.AreEqual(expected, regner.dividereFunktion(1000, 10));
+
+        }
+
+    }
+}
